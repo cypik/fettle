f707f8ec72d2c6e3a5f9a9886915004032789c9b
sohan yadav <sohanyadavdevops@gmail.com>
2024-12-07 11:15:22 +0530

    update health-check.yml

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -61,9 +61,9 @@ done
 if [[ $commit == true ]]
 then
   echo "committing logs"
-  git config --global user.name 'github-actions[bot]'
-  git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+  git config --global user.name ${user_name}
+  git config --global user.email ${user_email}
   git add -A --force public/status/
   git commit -am '[Automated] Update Health Check Logs'
-  git push
+  git push https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 fi
